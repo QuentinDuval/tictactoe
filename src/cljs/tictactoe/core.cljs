a948dda198291ecba03935a39bee3b6c9ab93921
Duval Quentin <senmenty@gmail.com>
2016-12-08 12:49:57 +0100

    first commit

--- src/cljs/tictactoe/core.cljs
+++ src/cljs/tictactoe/core.cljs
@@ -3,9 +3,74 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(def board-size 3)
 
-(defn greeting []
-  [:h1 (:text @app-state)])
+(def cell-state?
+  #{:cell/empty :cell/cross :cell/circle})
 
-(reagent/render [greeting] (js/document.getElementById "app"))
+(defn init-board
+  []
+  (into {}
+    (for [x (range board-size)
+          y (range board-size)]
+      [[x y] :cell/cross]
+      )))
+
+(defonce app-state
+  (atom {:board (init-board)}))
+
+
+(defmulti render-cell
+  "Draw a rectangle cell on the screen"
+  (fn [type x y] type))
+
+(defmethod render-cell :cell/empty
+  [_ x y]
+  [:rect
+   {:x (+ 0.05 x) :width 0.9
+    :y (+ 0.05 y) :height 0.9
+    :fill "lightgrey"
+    :on-click #(println "titi")}
+   ])
+
+(defmethod render-cell :cell/circle
+  [_ x y]
+  [:circle
+   {:cx (+ 0.5 x) :cy (+ 0.5 y)
+    :r 0.45 :fill "none"
+    :stroke-width 0.03
+    :stroke "green"}
+   ])
+
+(defn draw-cross-line
+  [[x y] [dx dy]]
+  [:line {:x1 x :y1 y
+          :x2 (+ x dx) :y2 (+ y dy)
+          :fill "none" :stroke-width 0.03 :stroke "red"}
+   ])
+
+(defmethod render-cell :cell/cross
+  [_ x y]
+  [:g
+   [draw-cross-line [x y] [1 1]]
+   [draw-cross-line [x (inc y)] [1 -1]]
+   ])
+
+
+(defn tictactoe
+  []
+  [:div
+   [:h1 "tic tac toe"]
+   (into
+     [:svg.board
+      {:view-box (str "0 0 " board-size " " board-size)
+       :style {:max-height "500px"}}]
+     (for [i (range board-size)
+           j (range board-size)]
+       [render-cell (get (:board @app-state) [i j]) i j]
+       ))
+   ])
+
+(reagent/render
+  [tictactoe]
+  (js/document.getElementById "app"))
