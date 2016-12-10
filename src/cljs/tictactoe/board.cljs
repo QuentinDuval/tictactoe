93b3831d19a651a26b8b59fb23b983d75fc1a6ed
Duval Quentin <senmenty@gmail.com>
2016-12-10 09:58:42 +0100

    extract the board

---
+++ src/cljs/tictactoe/board.cljs
@@ -0,0 +1,24 @@
+(ns tictactoe.board
+  (:require
+    [tictactoe.cell :as cell]
+    [tictactoe.logic :as logic]
+    ))
+
+
+(defn render-board-cell
+  "Render a cell on the board"
+  [board x y on-move]
+  (cell/render-cell
+    (get board [x y]) x y
+    {:on-click #(on-move x y)}))
+
+(defn render-board
+  "Render the board: pure component that only depends on inputs"
+  [board on-move]
+  (into
+    [:svg.board
+     {:view-box (str "0 0 " logic/board-size " " logic/board-size)
+      :style {:max-height "500px"}}]
+    (for [[x y] logic/coordinates]
+      [render-board-cell board x y])
+    ))
--- src/cljs/tictactoe/core.cljs
+++ src/cljs/tictactoe/core.cljs
@@ -1,7 +1,7 @@
 (ns tictactoe.core
   (:require
     [reagent.core :as reagent :refer [atom]]
-    [tictactoe.cell :as cell]
+    [tictactoe.board :as board]
     [tictactoe.panel :as panel]
     [tictactoe.logic :as logic])
   (:require-macros
@@ -23,33 +23,12 @@
 ;; Main rendering
 ;; --------------------------------------------------------
 
-(defn render-board-cell
-  "Render a cell on the board"
-  [board x y]
-  (cell/render-cell
-    (get board [x y]) x y
-    {:on-click #(on-move-event x y)}))
-
-(defn render-board
-  "Render the board: pure component that only depends on inputs"
-  [board]
-  (into
-    [:svg.board
-     {:view-box (str "0 0 " logic/board-size " " logic/board-size)
-      :style {:max-height "500px"}}]
-    (for [[x y] logic/coordinates]
-      [render-board-cell board x y])
-    ))
-
-(defn tictactoe
+(defn tic-tac-toe
   "Main rendering entry: assemble the game logic, the view and control"
   []
   [:div
    (panel/render-top-panel {:on-restart on-restart-event :on-undo on-undo-event})
-   (render-board @current-board)
+   (board/render-board @current-board on-move-event)
    ])
 
-
-;; Plug to reagent
-
-(reagent/render [tictactoe] (js/document.getElementById "app"))
+(reagent/render [tic-tac-toe] (js/document.getElementById "app"))
