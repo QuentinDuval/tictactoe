d1474bfc6e23c5e29e524eb79736026cf6cbb13c
Duval Quentin <senmenty@gmail.com>
2016-12-24 15:51:22 +0100

    unit tests import do not work in clojurescript...

--- test/cljs/tictactoe/test_runner.cljs
+++ test/cljs/tictactoe/test_runner.cljs
@@ -1,9 +1,10 @@
-(ns tictactoe.test-runner
+(ns cljs.tictactoe.test-runner
   (:require
+    ;;[tictactoe.common-test]
+    ;;[tictactoe.game-logic-test] ;; TODO - The import does not work. WHY?
     [cljs.test :as test]
-    ;;[tictactoe.game-logic-test :as logic-test]
-    ;; TODO - Import does not work... why ?
     ))
 
 (test/run-tests
+  'tictactoe.common-test
   'tictactoe.game-logic-test)
