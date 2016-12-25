52c9b2a8267bae2ab71425619a06b7b8d8bcd218
Duval Quentin <senmenty@gmail.com>
2016-12-25 18:58:22 +0100

    simplify the board rendering

--- src/cljs/tictactoe/view/board.cljs
+++ src/cljs/tictactoe/view/board.cljs
@@ -6,13 +6,18 @@
     ))
 
 
+(defn svg-board-plane
+  [height-in-cells height-in-pixel]
+  [:svg.board
+   {:view-box (str "0 0 " height-in-cells " " height-in-cells)
+    :style {:max-height (str height-in-pixel "px")}}
+   ])
+
 (defn render-board
   "Render the board: pure component that only depends on inputs"
   [board on-move]
   (into
-    [:svg.board
-     {:view-box (str "0 0 " logic-cst/board-size " " logic-cst/board-size)
-      :style {:max-height (str cst/board-pixel-size "px")}}]
+    (svg-board-plane logic-cst/board-size cst/board-pixel-size)
     (for [[[x y] cell-owner] board]
-      [cell/render-cell cell-owner x y {:on-click #(on-move x y)}])
+      (cell/render-cell cell-owner x y {:on-click #(on-move x y)}))
     ))
