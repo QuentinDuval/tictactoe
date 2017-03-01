8d280eec7a3fdaa49d82b856e2db72105795b8cb
Duval Quentin <senmenty@gmail.com>
2017-03-01 21:43:17 +0100

    renaming

--- src/cljs/tictactoe/store.cljs
+++ src/cljs/tictactoe/store.cljs
@@ -1,7 +1,7 @@
 (ns tictactoe.store
   (:require
     [reagent.core :as reagent]
-    [tictactoe.logic.game :as logic])
+    [tictactoe.logic.game :as game])
   (:require-macros
     [reagent.ratom :refer [reaction]]))
 
@@ -10,6 +10,6 @@
 ;; Game state (game loop: use event to trigger logic)
 ;; --------------------------------------------------------
 
-(defonce app-state (reagent/atom (logic/new-game)))
-(def current-turn (reaction (logic/current-turn @app-state)))
-(defn send-event! [e] (swap! app-state logic/handle-event e))
+(defonce app-state (reagent/atom (game/new-game)))
+(def current-turn (reaction (game/current-turn @app-state)))
+(defn send-event! [e] (swap! app-state game/handle-event e))
